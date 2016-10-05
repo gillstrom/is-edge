b4aeec01783feea5550d64920db3405cd2732c3e
Andreas Gillström <andreasgillstrom@gmail.com>
2016-10-05 17:52:19 +0200

    Add tests

---
+++ test.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import m from './';
+
+test(t => {
+	t.false(m());
+	t.true(m({userAgent: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'}));
+	t.true(m({version: 12, userAgent: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'}));
+	t.true(m({version: '12.10136', userAgent: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'}));
+	t.false(m({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36 OPR/29.0.1795.60'}));
+});
